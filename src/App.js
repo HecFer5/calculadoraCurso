250d10fb6bbd83d2c5f78378bb8b25d2f0327df1
HecFer5 <hecalfer@hotmail.com>
2023-04-28 16:57:03 -0300

    se agregan filas

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
     alt='logo de freeCodeCamp' />
     </div>
     <div className='contenedor-calculadora'>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
 
     </div>
     </div>
