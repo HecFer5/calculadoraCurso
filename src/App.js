a40129fa3d97a52ccf7efe62254fc4c8aff47646
HecFer5 <hecalfer@hotmail.com>
2023-04-28 15:51:51 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Logo from './imagenes/FreeCodeCamp_logo.png'
+
+function App() {
+  return (
+    <div className='App'>
+   <div className='logo-contenedor'>
+    <img src={Logo} 
+    className='logo'
+    alt='logo de freeCodeCamp' />
+    </div>
+    <div className='contenedor-calculadora'>
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
