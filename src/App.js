b59566ecc454aea4ad6be229e32c7b6e5c6f7157
HecFer5 <hecalfer@hotmail.com>
2023-04-29 17:02:03 -0300

    agrego pantalla y botones

--- src/App.js
+++ src/App.js
@@ -2,28 +2,45 @@
 import './App.css';
 import Logo from './imagenes/FreeCodeCamp_logo.png'
 import Boton from './componentes/boton';
+import Pantalla from './componentes/Pantalla';
 
 function App() {
   return (
     <div className='App'>
-   <div className='logo-contenedor'>
-    <img src={Logo} 
-    className='logo'
-    alt='logo de freeCodeCamp' />
-    </div>
-    <div className='contenedor-calculadora'>
-      <div className='fila'>
+      <div className='logo-contenedor'>
+        <img src={Logo}
+          className='logo'
+          alt='logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla />
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
         <Boton>1</Boton>
-        <Boton>+</Boton>
-      </div>
-      <div className='fila'>
-      <Boton>*</Boton>
-      </div>
-      <div className='fila'></div>
-      <div className='fila'></div>
-      <div className='fila'></div>
+        <Boton>2</Boton>
+        <Boton>3</Boton>
+        <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+        <Boton>=</Boton>
+        <Boton>0</Boton>
+        <Boton>.</Boton>
+        <Boton>/</Boton>
+        </div>
+        <div className='fila'></div>
 
-    </div>
+      </div>
     </div>
   );
 }
